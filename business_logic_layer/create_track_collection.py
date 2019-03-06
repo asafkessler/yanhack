6ea4b1a27bab1632d346e0daa33c339862d44720
ilan levi <ilan_levi@hotmail.com>
2019-03-06 13:22:09 +0200

    added const

--- business_logic_layer/create_track_collection.py
+++ business_logic_layer/create_track_collection.py
@@ -1,8 +1,18 @@
 from data_access_layer.models.track import Track
 from data_access_layer.models.statevector import StateVector
+import utils.csv_files_handler as csv_files_handler
+import utils.data_parser as parser
 
 
-def create_track_collection_from_db():
+def track_collection_create_in_db():
+    file_path = csv_files_handler.get_relevant_file_path()
+    tracks_csv = parser.create_csv_based_db_collection(file_path)
+    t_map = create_track_collection_from_db(tracks_csv)
+    t_map['bla']
+
+
+
+def create_track_collection_from_db(tracks_csv):
     tracks_map = {}
 
     def handle_track(track_from_db):
@@ -18,6 +28,6 @@ def create_track_collection_from_db():
         track.add_to_state_vector_list(state_v)
         tracks_map[track.getId()] = track
 
-
+    return tracks_map
 
 
